f820b389bb3bf83916412939aa101e11f1fc6dd0
deluxezzz <deluxez@mail.ru>
2020-04-05 22:53:30 +0300

    +slider

---
+++ scripts/slider.js
@@ -0,0 +1,21 @@
+document.getElementById('button-left').onclick = buttonLeft;
+document.getElementById('button-right').onclick = buttonRight;
+
+let left = 0;
+let lots = document.getElementById('lots');
+
+function buttonRight() {
+    left = left - 317;
+    if(left < -951) {
+        left = 0;
+    }
+    lots.style.left = left +'px';
+}
+
+function buttonLeft () {
+    left = left + 317;
+    if(left > 0) {
+        left = -951;
+    }
+    lots.style.left = left +'px';
+}
